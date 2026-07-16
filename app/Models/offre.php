466783f4896476c72606cbc348fb6fa033e6b47c
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-07-16 12:21:54 +0100

    Modéliser l'offre et ces relations

--- app/Models/offre.php
+++ app/Models/offre.php
@@ -4,7 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class offre extends Model
+class Offre extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'description',
+        'budget',
+        'user_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function candidatures()
+    {
+        return $this->hasMany(Candidature::class);
+    }
 }
