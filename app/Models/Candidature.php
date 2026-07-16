bf47b30fc60c86b7bb2574fe3e2c942541e889ed
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-07-16 12:30:27 +0100

    Modéliser Candidature et ces relations

---
+++ app/Models/Candidature.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Candidature extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'offre_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function offre()
+    {
+        return $this->belongsTo(Offre::class);
+    }
+}
