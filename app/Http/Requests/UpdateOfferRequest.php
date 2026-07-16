1d351af7b19e9631cec2a73829c81307c992ae9d
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-07-16 13:05:37 +0100

    validation de update d'une offre

--- app/Http/Requests/UpdateOfferRequest.php
+++ app/Http/Requests/UpdateOfferRequest.php
@@ -5,25 +5,24 @@ namespace App\Http\Requests;
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
 class UpdateOfferRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'budget' => 'required|numeric|min:0',
         ];
     }
 }
