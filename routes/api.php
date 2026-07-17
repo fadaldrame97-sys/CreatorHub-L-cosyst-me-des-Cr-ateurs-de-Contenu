a749ca7750118444eb20fcbbe04b9df474f2b43d
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-07-17 11:59:53 +0100

    Ajouter une route générale pour toute les fonctions de candidatures

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\TestController;
 use App\Http\Controllers\Api\OfferController;
+use App\Http\Controllers\Api\CandidatureController;
+
 
 Route::get('/test', [TestController::class, 'index']);
 Route::get('/offres', [OfferController::class, 'index']);
 
 Route::apiResource('offres', OfferController::class);
+Route::apiResource('candidatures', CandidatureController::class);
